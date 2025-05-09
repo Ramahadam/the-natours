659eb6c87af7e6e80655f03975f5d0f8db9b7063
RamahAdam <ramy.adam33@gmail.com>
2025-05-09 17:11:14 +0400

    creating endpoint for tours and tested in postmon

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Request from the server', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this URL');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App is runnning on port ${port}`);
+});
---
+++ legacy_nodejs.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/') res.end('Homepage');
+  else if (pathName === '/about') res.end('About page');
+  else res.end('Page not found');
+});
+
+server.listen(3001, (req, res) => {
+  console.log('Listening for incomming request');
+});
