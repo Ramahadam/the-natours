0df030c5b808179279e48f9433c80b0ccedbdbb7
Mohamed <ramy.adam33@gmail.com>
2025-06-19 00:23:19 +0400

    Implement functionality which allow the user to update his data (password & data)

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,18 @@ const signToken = (id) => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -23,15 +35,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordChangedAt: req.body.passwordChangedAt,
   });
 
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -50,13 +54,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Invalid user or password', 401));
 
   // 01) If everything is ok, send token to client
-
-  const token = signToken(user._id);
-
-  res.status(200).json({
-    status: 'sccuess',
-    token,
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -181,9 +179,31 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // log the user in, send jwt.
 
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'sccuess',
-    token,
-  });
+  createSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // As security measure we always ask for current password before updating it.
+  // 01.Get the user from the collection
+  const { currentPassword, newPassword, passwordConfirm } = req.body;
+
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 02. Check if the posted password current password is correct.
+  const isCorrectPassword = await user.correctPassword(
+    currentPassword,
+    user.password,
+  );
+
+  if (!user || !isCorrectPassword)
+    return next(new AppError('The current password is not correct', 401));
+
+  // 03. Update the password.
+  user.password = newPassword;
+  user.passwordConfirm = passwordConfirm;
+
+  await user.save();
+
+  // 04. Log user is, send JWT to the user
+  createSendToken(user, 200, res);
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -10,6 +23,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // Create error if user post password data
+  if (req.body.password || req.body.passwordConfirm)
+    return next(
+      new AppError(
+        'This is not password update endpoint please use /updateMyPassword',
+        400,
+      ),
+    );
+
+  // update user document.
+
+  // Filterout unwanted fields name which are not allowed to be updated
+  const filterBody = filterObj(req.body, 'name', 'email');
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
