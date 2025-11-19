cabbcd24d37c4e8e1a39ed380684ae0b9e0c31d7
Mohamed <ramy.adam33@gmail.com>
2025-11-19 23:23:19 +0400

    feat: add getMe controller to retrieve current user information

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,6 +17,11 @@ const filterObj = (obj, ...allowedFields) => {
 
 exports.getAllUsers = factory.getAll(User);
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // Create error if user post password data
   if (req.body.password || req.body.passwordConfirm)
