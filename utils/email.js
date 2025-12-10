c8f992ac7a15712b8aca1680178cca02cd03c0a7
Mohamed <ramy.adam33@gmail.com>
2025-12-10 14:36:46 +0400

    feat: configure email class for sending emails

--- utils/email.js
+++ utils/email.js
@@ -1,29 +1,65 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (options) => {
-  // 01. Create a transporter
+const { convert } = require('html-to-text');
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+const pug = require('pug');
 
-  // 02. Define email options.
-  const mailOptions = {
-    from: 'Mohamed Adam <hello@tes.io>',
-    to: options.email,
-    subject: options.subject,
-    text: 'Plaintext version of the message',
-    html: `<p>${options.message}</p>`,
-  };
+module.exports = class Email {
+  constructor(user, url) {
+    this.name = user.name.split(' ')[0];
+    this.to = user.email;
+    this.url = url;
+    this.from = `Mohamed Adam <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 03. Send the actual email with nodemailr
-  await transporter.sendMail(mailOptions);
+  // create transport based on enviroment
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Send real eamil to customer in production using sendgrid
+      return 1;
+    }
+
+    // Send mailtrap in development
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send given template
+  async send(template, subject) {
+    // Prepare the HTML file using pug
+
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.name,
+        url: this.url,
+        subject,
+      },
+    );
+
+    // 02. Define email options.
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html),
+    };
+
+    // 03. Send the actual email with nodemailr
+    await this.newTransport.sendMail(mailOptions);
+  }
+
+  // Send welcome email
+  async sendWelcome() {
+    await this.send('welcome', 'Weclome to natours family');
+  }
 };
-
-module.exports = sendEmail;
